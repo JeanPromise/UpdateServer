aa236aaf499d7b82d1ab3ab2e2234cb4ae1ad6bb
JeanPromise <jeanpromise6@gmail.com>
2025-09-12 21:01:58 +0300

    app.py

--- app.py
+++ app.py
@@ -211,4 +211,6 @@ def delete_apk():
 
 # ---------------- Run ----------------
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # For local testing only
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
