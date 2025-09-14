d113876be7a8c386e21c293677c5d4a970e8a4ed
JeanPromise <jeanpromise6@gmail.com>
2025-09-14 21:39:52 +0300

    app.py

--- app.py
+++ app.py
@@ -241,5 +241,5 @@ def get_messages():
 
 # ---------------- Run ----------------
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000"))  # Render gives you $PORT
+    port = int(os.environ.get("PORT", 5000))  # Render gives you $PORT
     app.run(host="0.0.0.0", port=port, debug=False)
