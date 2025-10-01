2764f5151ff34d2b5bddc5d8869280fae9c03ef1
JeanPromise <jeanpromise6@gmail.com>
2025-10-01 20:55:01 +0300

    app.py

--- app.py
+++ app.py
@@ -479,4 +479,9 @@ def admin_delete_user():
 # ---------------- Run ----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False,
+        use_reloader=False
+    )
